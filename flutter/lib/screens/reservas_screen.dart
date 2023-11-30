9aecf23972c4c3bf91b6c63fe8309741ae1df35a
JoseDonosoValds <jdonoso@utem.cl>
2023-11-30 19:21:46 -0300

    configuracion de botones. Cambio de color de botones, ancho y largo.

--- flutter/lib/screens/reservas_screen.dart
+++ flutter/lib/screens/reservas_screen.dart
@@ -15,30 +15,42 @@ class ReservasScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar "Salas Disponibles"
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SalasScreen(),
-                  ),
-                );
-              },
-              child: Text('Ver Salas'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60.0), 
+              child: ElevatedButton(
+                onPressed: () {
+                  // Acción al presionar "Salas Disponibles"
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SalasScreen(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50), 
+                ),
+                child: Text('Ver Salas'),
+              ),
             ),
             SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar "Reservas"
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MisReservasScreen(),
-                  ),
-                );
-              },
-              child: Text('Mis Reservas'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60.0), 
+              child: ElevatedButton(
+                onPressed: () {
+                  // Acción al presionar "Reservas"
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MisReservasScreen(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50), 
+                ),
+                child: Text('Mis Reservas'),
+              ),
             ),
           ],
         ),
@@ -60,53 +72,69 @@ class MisReservasScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar "buscar reserva"
-                print('Buscar Reservas presionado');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(71, 123, 212, 126), 
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60.0), 
+              child: ElevatedButton(
+                onPressed: () {
+                  // Acción al presionar "buscar reserva"
+                  print('Buscar Reservas presionado');
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50), 
+                  primary: Color.fromARGB(70, 43, 206, 28),
+                ),
+                child: Text('Buscar Reserva'),
               ),
-              child: Text('Buscar Reserva'),
             ),
             SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar "Crear Reserva"
-                print('Crear Reserva presionado');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(71, 123, 212, 126), 
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60.0), 
+              child: ElevatedButton(
+                onPressed: () {
+                  // Acción al presionar "Crear Reserva"
+                  print('Crear Reserva presionado');
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50), 
+                  primary: Color.fromARGB(70, 43, 206, 28),
+                ),
+                child: Text('Crear Reserva'),
               ),
-              child: Text('Crear Reserva'),
             ),
             SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar "buscar reserva por código"
-                print('Buscar Reservas por codigo presionado');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(70, 81, 55, 230), // Cambia el color de fondo del botón
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60.0), 
+              child: ElevatedButton(
+                onPressed: () {
+                  // Acción al presionar "buscar reserva por código"
+                  print('Buscar Reservas por código presionado');
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50), 
+                  primary: Color.fromARGB(70, 40, 28, 206),
+                ),
+                child: Text('Buscar Reserva por código'),
               ),
-              child: Text('Buscar Reserva por codigo'),
             ),
             SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar "eliminar reserva"
-                print('Eliminar Reserva presionado');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(70, 206, 28, 28), // Cambia el color de fondo del botón
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60.0), 
+              child: ElevatedButton(
+                onPressed: () {
+                  // Acción al presionar "eliminar reserva"
+                  print('Eliminar Reserva presionado');
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50),
+                  primary: Color.fromARGB(70, 206, 28, 28), 
+                ),
+                child: Text('Eliminar Reserva'),
               ),
-              child: Text('Eliminar Reserva'),
             ),
             SizedBox(height: 16),
-            ]
+          ],
+        ),
       ),
-    ),
     );
   }
 }
@@ -124,31 +152,39 @@ class SalasScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar "buscar sala por codigo"
-                print('Buscar sala por codigo');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(70, 81, 55, 230), // Cambia el color de fondo del botón
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 80.0), 
+              child: ElevatedButton(
+                onPressed: () {
+                  // Acción al presionar "buscar sala por código"
+                  print('Buscar sala por código');
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50), 
+                  primary: Color.fromARGB(70, 40, 28, 206), 
+                ),
+                child: Text('Buscar sala por código'),
               ),
-              child: Text('Buscar sala por codigo'),
             ),
             SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar "Mostrar salas"
-                print('Mostrar salas');
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(70, 81, 55, 230), // Cambia el color de fondo del botón
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 80.0), 
+              child: ElevatedButton(
+                onPressed: () {
+                  // Acción al presionar "Mostrar salas"
+                  print('Mostrar salas');
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50), 
+                  primary: Color.fromARGB(70, 40, 28, 206),
+                ),
+                child: Text('Mostrar salas'),
               ),
-              child: Text('Mostrar salas'),
             ),
             SizedBox(height: 16),
-            ]
+          ],
+        ),
       ),
-    ),
     );
   }
 }
