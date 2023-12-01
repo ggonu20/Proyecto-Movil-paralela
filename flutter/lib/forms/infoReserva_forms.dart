e46520809dfdaae330a89ff6cf44f65eda22937a
ggonu20 <g.gonzalezn@utem.cl>
2023-12-01 02:34:40 -0300

    01/12 2:34
    Creado forms info reserva
    
    Co-Authored-By: JoseDonosoValds <67485758+JoseDonosoValds@users.noreply.github.com>

---
+++ flutter/lib/forms/infoReserva_forms.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:cpyd/services/google_service.dart'; //importar google service
+import 'package:cpyd/services/reservas.dart'; //importar reservas
+
+class FormsreserveInfo extends StatefulWidget {
+  final Function(String, String) onSubmit;
+
+  const FormsreserveInfo({super.key, required this.onSubmit});
+
+  @override
+  _FormsScreenState createState() => _FormsScreenState();
+}
+
+class _FormsScreenState extends State<FormsreserveInfo> {
+  final TextEditingController roomCodeController = TextEditingController();
+  final TextEditingController startController = TextEditingController();
+
+  String? roomCodeValidator(String? value){
+    if (value == null || value.isEmpty){
+      return 'Ingrese el Room Code';
+    }
+    return null;
+  }
+
+  String? startValidator(String? value){
+    if (value == null || value.isEmpty){
+      return 'Ingrese el start';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ingrese Datos'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: roomCodeController,
+              decoration: const InputDecoration(labelText: 'Room Code'),
+              validator: roomCodeValidator,
+            ),
+            TextFormField(
+              controller: startController,
+              decoration: const InputDecoration(labelText: 'Start'),
+              validator: startValidator,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                String roomCode = roomCodeController.text;
+                String start = startController.text;
+
+                // Llamada a la función onSubmit del widget padre
+                widget.onSubmit(roomCode, start);
+                String jwt = await GoogleService.getData('idToken');
+                List<dynamic> respuesta = await ApiReserve.infoReserva(jwt, roomCode, start);
+
+                // Mostrar el widget ReservasWidget después de enviar los datos
+                // ignore: use_build_context_synchronously
+                /*Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ReservasWidget(reservas: respuesta),
+                  ),
+                );*/
+              },
+              child: const Text('Enviar Datos'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
